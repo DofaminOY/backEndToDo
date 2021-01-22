7ad7d12331cec8bf591ab99d52b306241b30db15
Yura O <yurkasoft@gmail.com>
2021-01-22 18:57:41 +0200

    router+

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+async function start() {
+    try {
+      await mongoose.connect('mongodb+srv://blablasoft:qcwdeeqcwdqc@cluster0.gr1e5.mongodb.net/back-end',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        
+          useFindAndModify: false
+      }
+      )
+      app.listen(PORT, () => {
+        console.log('Server has been started...')
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
+  start()
